34fe430af08073b59f19bcd24ad9864ea46e2dd2
Mehedee Hassan <razin.meha@gmail.com>
2016-12-23 16:12:53 +0600

    error on touch

--- MiraclePill/MiraclePill/ViewController.swift
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -8,18 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewDataSource{
 
+    
+ 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var stateButton: UIButton!
+    
+    
+    let states = ["Dhaka","Rangpur","barishal","chittagong"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
+    
+    
 
+    
+    
+    // implementation
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        
+        // column
+        return 1
+    }
+    
+    
+ 
+    
+   public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+   {
+      return 0//states.count
+   }
+    
+    
+    
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+        
+    {
+        //stateButton.setTitle(states[row], for: UIControlState())
+        //statePicker.isHidden = true
+        return ""//states[row]
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
-
+    @IBAction func onStateButtonClick(_ sender: AnyObject) {
+        statePicker.isHidden = false
+    }
+  
 }
 
